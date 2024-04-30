a3c95ba8dfa86f4295b3db87545db0d0f06bfb6f
Al Samir <shahinur1591@gmail.com>
2024-05-01 00:56:50 +0600

    Registation done

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,8 +1,57 @@
+import { useContext } from "react";
 import { useForm } from "react-hook-form";
-
+import { AuthContext } from "../Provider/AuthProvider";
+import Swal from "sweetalert2";
+import { auth } from "../config/firebase.config";
+import { updateProfile } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 const Register = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const navigate = useNavigate();
+  const { createUser } = useContext(AuthContext);
+  const onSubmit = (data, event) => {
+    const { name, email, photoURL, password } = data;
+    const isValidPassword = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/.test(password);
+    if (isValidPassword) {
+      createUser(email, password)
+        .then(() => {
+          updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photoURL,
+          })
+            .then(() => {
+              Swal.fire({
+                title: "Successfully Registered",
+                icon: "success",
+                confirmButtonText: "Close",
+              });
+              event.target.reset();
+              navigate("/home");
+            })
+            .catch((err) => console.error(err));
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          Swal.fire({
+            title: "Error!",
+            text: `${errorCode} ${errorMessage}`,
+            icon: "error",
+            confirmButtonText: "Cool",
+          });
+        });
+    } else {
+      Swal.fire({
+        title: "Password Validation",
+        text: `Must have an Uppercase letter in the password, 
+              Must have a Lowercase letter in the password,
+              Length must be at least 6 character
+              `,
+        icon: "error",
+        confirmButtonText: "Close",
+      });
+    }
+  };
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse w-full">
