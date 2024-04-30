91d386c268ab8212952f7d307b0c4974eef571ff
Al Samir <shahinur1591@gmail.com>
2024-04-30 23:26:02 +0600

    Client site setup done

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const apiRoutes = require('./routes/api')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const apiRoutes = require("./routes/api");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.DATABASE_URI)
-.then(console.log("Connected to Database"))
-.catch(error => console.log(error));
+mongoose
+  .connect(process.env.DATABASE_URI)
+  .then(console.log("Connected to Database"))
+  .catch((error) => console.log(error));
 
 app.use(apiRoutes);
 
