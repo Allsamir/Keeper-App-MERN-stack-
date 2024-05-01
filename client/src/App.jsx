fd0aea81af51a9142c2273c8ec0400c6de2ed42a
Al Samir <shahinur1591@gmail.com>
2024-05-01 20:57:42 +0600

    Update func setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -51,6 +51,12 @@ function App() {
       }
     });
   };
+  const showModal = () => {
+    document.getElementById("my_modal_1").showModal();
+  };
+  const updateNote = () => {
+    console.log("Hello World");
+  };
   return (
     <>
       <Navbar />
@@ -63,9 +69,31 @@ function App() {
         )}
         <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-8">
           {notes.map((note, index) => (
-            <Notes key={index} note={note} handleDelete={handleDelete} />
+            <Notes
+              key={index}
+              note={note}
+              handleDelete={handleDelete}
+              showModal={showModal}
+            />
           ))}
         </div>
+        {/* Open the modal using document.getElementById('ID').showModal() method */}
+        <dialog id="my_modal_1" className="modal">
+          <div className="modal-box">
+            <h3 className="font-bold text-lg">Hello!</h3>
+            <p className="py-4">
+              Press ESC key or click the button below to close
+            </p>
+            <div className="modal-action">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn" onClick={updateNote}>
+                  Close
+                </button>
+              </form>
+            </div>
+          </div>
+        </dialog>
       </div>
       <Footer />
     </>
--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -1,12 +1,16 @@
 import PropTypes from "prop-types";
 import { MdDelete } from "react-icons/md";
+import { HiOutlinePencilSquare } from "react-icons/hi2";
 
-const Notes = ({ note, handleDelete }) => {
+const Notes = ({ note, handleDelete, showModal }) => {
   return (
     <div className="bg-white text-black p-4 font-pop rounded-lg">
       <h1 className="text-2xl font-bold font-lobStar pb-3">{note.title}</h1>
       <p className="text-base">{note.note}</p>
-      <div className="actions flex justify-end mt-4">
+      <div className="actions flex justify-end mt-4 gap-3">
+        <button onClick={showModal}>
+          <HiOutlinePencilSquare className="text-xl hover:text-green-500" />
+        </button>
         <button onClick={() => handleDelete(note._id)}>
           <MdDelete className="text-xl hover:text-red-600" />
         </button>
@@ -17,5 +21,6 @@ const Notes = ({ note, handleDelete }) => {
 Notes.propTypes = {
   note: PropTypes.object.isRequired,
   handleDelete: PropTypes.func.isRequired,
+  showModal: PropTypes.func.isRequired,
 };
 export default Notes;
